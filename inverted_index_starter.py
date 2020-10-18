5a369a00b0b80a116e5d7fd401720c55cf370b24
RomanMatiiv <matiiv.r.a@gmail.com>
2020-10-18 18:55:28 +0300

    implenemt inverting index and write test

--- inverted_index_starter.py
+++ inverted_index_starter.py
@@ -1,22 +1,67 @@
+import json
+
+
 class InvertedIndex:
+    def __init__(self):
+        self.word_in_docs = {}
+
     def query(self, words: list) -> list:
         """Return the list of relevant documents for the given query"""
-        pass
+        documents = []
+
+        for word in words:
+            try:
+                documents.extend(self.word_in_docs[word])
+            except KeyError:
+                pass # если искомое слово отсутствует в индексе
+
+        documents = list(set(documents))
+
+        return documents
+
 
     def dump(self, filepath: str):
-        pass
+        with open(filepath, "w") as file:
+            json.dump(self.word_in_docs, file)
 
     @classmethod
     def load(cls, filepath: str):
-        pass
+        inverted_index = cls()
+
+        with open(filepath, "r") as file:
+            inverted_index.word_in_docs = json.load(file)
+
+        return inverted_index
 
 
-def load_documents(filepath: str):
-    pass
+def load_documents(filepath: str) -> list:
+    documents = []
+
+    with open(filepath) as fin:
+        for row in fin.readlines():
+            doc_id, text = (row.strip()
+                               .split('\t'))
+            doc_id = int(doc_id)
+            documents.append([doc_id, text])
+
+    return documents
 
 
-def build_inverted_index(documents):
-    pass
+def build_inverted_index(documents: list):
+    inverted_index = InvertedIndex()
+
+    for doc_id, doc in documents:
+        for word in doc.split():
+            if word in inverted_index.word_in_docs.keys():
+                inverted_index.word_in_docs[word].append(doc_id)
+            else:
+                inverted_index.word_in_docs[word] = [doc_id]
+
+    # Удаление дубликатов
+    for key in inverted_index.word_in_docs.keys():
+        inverted_index.word_in_docs[key] = list(set(inverted_index.word_in_docs[key]))
+
+    return inverted_index
 
 
 def main():
---
+++ test_inverted_index.py
@@ -0,0 +1,123 @@
+import os
+
+import pytest
+
+import inverted_index_starter as IIS
+
+
+def test_load_documents_on_doesnt_exist_file():
+    path_to_doesnt_exist_file = "fake/path/to/dataset.txt"
+
+    with pytest.raises(FileNotFoundError):
+        IIS.load_documents(path_to_doesnt_exist_file)
+
+
+def test_load_documents_on_2_doc():
+    docs = "1\tHello\n2\tworld\n"
+
+    expected_docs = [[1, "Hello"],
+                     [2, "world"]]
+
+    with File(content=docs) as f:
+        load_docs = IIS.load_documents(f.file_path)
+
+        assert expected_docs == load_docs
+
+
+def test_load_documents_on_incorrect_doc_struct():
+    docs = "1\tHello\n2\tworld\tworld\n"
+
+    with pytest.raises(ValueError):
+        with File(content=docs) as f:
+            IIS.load_documents(f.file_path)
+
+
+def test_build_inverted_index():
+
+    expect_inverted_index = {"hello": [1, 2],
+                             "world": [2]}
+
+    docs = [[1, "hello"],
+            [2, "hello  world"]]
+    inverted_index = IIS.build_inverted_index(docs)
+
+    assert expect_inverted_index == inverted_index.word_in_docs
+
+
+def test_dump_inverted_index():
+    inverted_index = IIS.InvertedIndex()
+
+    inverted_index.word_in_docs = {"hello": [1, 2],
+                                   "world": [2]}
+
+    inverted_index.dump("tmp.json")
+
+    os.remove("tmp.json")
+
+
+def test_dump_and_load_inverted_index():
+
+    expected ={"hello": [1, 2],
+               "world": [2]}
+
+    inverted_index = IIS.InvertedIndex()
+
+    inverted_index.word_in_docs = expected
+
+    inverted_index.dump("tmp.json")
+    del inverted_index
+
+    inverted_index = IIS.InvertedIndex.load("tmp.json")
+
+    os.remove("tmp.json")
+
+    assert expected == inverted_index.word_in_docs
+
+
+def test_query_in_inverted_index():
+    inverted_index = IIS.InvertedIndex()
+    inverted_index.word_in_docs = {"word": [1, 4],
+                                   "hello": [1, 2],
+                                   "covid": [3, 4]}
+
+    expect_docs = [1, 2, 4]
+    docs = inverted_index.query(["hello", "word"])
+
+    assert len(expect_docs) == len(docs)
+
+    # проверка на то, что все документы найдены
+    for doc in docs:
+        assert doc in expect_docs
+
+
+def test_query_not_in_inverted_index():
+    inverted_index = IIS.InvertedIndex()
+    inverted_index.word_in_docs = {"word": [1, 4],
+                                   "hello": [1, 2],
+                                   "covid": [3, 4]}
+
+    expect_docs = []
+    docs = inverted_index.query(["ABS", "DDDDD"])
+
+    assert len(expect_docs) == len(docs)
+
+
+class File:
+    def __init__(self, content, file_path="file_for_test.txt"):
+        self.file_path = file_path
+        self.content = content
+
+        self._write_to_file(content)
+
+    def _write_to_file(self, content):
+        with open(self.file_path, "w") as fout:
+            fout.write(content)
+
+    def _remove_file(self):
+        os.remove(self.file_path)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._remove_file()
