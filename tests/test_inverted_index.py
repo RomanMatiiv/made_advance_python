e023580cafebe3f463dd85295e496bd1cded6e80
RomanMatiiv <matiiv.r.a@gmail.com>
2020-10-27 08:22:48 +0300

    move test for storage_policy

--- tests/test_inverted_index.py
+++ tests/test_inverted_index.py
@@ -1,13 +1,8 @@
-import json
 import os
 
 import pytest
-import zlib
 
 import inverted_index as IIS
-from storage_policy import JsonStoragePolicy
-from storage_policy import PklStoragePolicy
-from storage_policy import ZlibStoragePolicy
 
 
 @pytest.fixture()
@@ -25,16 +20,6 @@ def small_dataset(tmpdir) -> dict:
     expect["file_with_raw_docs"] = file.strpath
 
     return expect
-
-
-@pytest.fixture()
-def sample_inverted_index(tmpdir):
-    inverted_index = {"hello": [1, 4],
-                      "world": [1],
-                      "I": [3, 4],
-                      "am": [2],
-                      "human": [2, 3]}
-    return inverted_index
 
 
 def test_load_documents_on_doesnt_exist_file():
@@ -130,58 +115,3 @@ def test_query_not_in_inverted_index():
     docs = inverted_index.query(["ABS", "DDDDD"])
 
     assert len(expect_docs) == len(docs)
-
-
-def test_json_storage_policy(tmpdir, sample_inverted_index):
-    expect = sample_inverted_index
-
-    path_to_dump = tmpdir.join("dump_tmp").strpath
-
-    JsonStoragePolicy.dump(expect, path_to_dump)
-
-    real = JsonStoragePolicy.load(path_to_dump)
-
-    assert expect == real
-
-
-def test_pkl_storage_policy(tmpdir, sample_inverted_index):
-    expect = sample_inverted_index
-
-    path_to_dump = tmpdir.join("dump_tmp").strpath
-
-    PklStoragePolicy.dump(expect, path_to_dump)
-
-    real = PklStoragePolicy.load(path_to_dump)
-
-    assert expect == real
-
-
-def test_zlib_storage_policy(tmpdir, sample_inverted_index):
-    expect = sample_inverted_index
-
-    path_to_dump = tmpdir.join("dump_tmp").strpath
-
-    ZlibStoragePolicy.dump(expect, path_to_dump)
-
-    real = ZlibStoragePolicy.load(path_to_dump)
-
-    assert expect == real
-
-
-def test_zlib_storage_policy_compress_level(tmpdir, sample_inverted_index):
-    expect = sample_inverted_index
-
-    path_to_dump = tmpdir.join("dump_tmp").strpath
-
-    ZlibStoragePolicy.dump(expect, path_to_dump, level=9)
-
-    real = ZlibStoragePolicy.load(path_to_dump)
-
-    assert expect == real
-
-
-def test_zlib_storage_policy_raise_compress_level(tmpdir, sample_inverted_index):
-    path_to_dump = tmpdir.join("dump_tmp").strpath
-
-    with pytest.raises(zlib.error):
-        ZlibStoragePolicy.dump(sample_inverted_index, path_to_dump, level=10)
