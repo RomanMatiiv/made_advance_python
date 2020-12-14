3ccb5742ddc5268735380768a426ec2708927d1a
RomanMatiiv <matiiv.r.a@gmail.com>
2020-12-14 07:59:09 +0300

    start work on stackoverflow_analytics project

---
+++ stackoverflow_analytics/pipeline.py
@@ -0,0 +1,7 @@
+
+class Pipeline:
+    def __init__(self):
+        raise NotImplementedError
+
+    def read_posts(self, path_to_file):
+        raise NotImplementedError
---
+++ stackoverflow_analytics/post.py
@@ -0,0 +1,10 @@
+
+class Post:
+    def __init__(self):
+        raise NotImplementedError
+
+
+class Word:
+    def __init__(self, word, score):
+        self.word = word
+        self.score = score
