8f99fa0575abf73f227e1fd9f84fec534992474f
RomanMatiiv <matiiv.r.a@gmail.com>
2020-12-16 08:14:38 +0300

    write pytest fixture for posts

--- stackoverflow_analytics/tests/test_pipeline.py
+++ stackoverflow_analytics/tests/test_pipeline.py
@@ -2,12 +2,15 @@ import pytest
 
 from pipeline import Pipeline
 
-@pytest.fixture()
-def stackoverflow_posts():
-    posts = """
-            <row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="10" Title="Is SEO better better better done with repetition?" />	
-            <row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="5" Title="What is SEO?" />	
-            <row PostTypeId="1" CreationDate="2020-01-15T20:31:15.640" Score="20" Title="Is Python better than Javascript?" />	
-            """
+@pytest.fixture(scope='function')
+def stackoverflow_posts_path(tmpdir):
+    posts =['<row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="10" Title="Is SEO better better better done with repetition?" />',
+            '<row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="5" Title="What is SEO?" />',
+            '<row PostTypeId="1" CreationDate="2020-01-15T20:31:15.640" Score="20" Title="Is Python better than Javascript?" />',
+            ]
+    post_tmp_file = tmpdir.join("posts.txt")
 
-    # TODO сгенерить на основе текста выше временный xml файл
+    for post in posts:
+        post_tmp_file.write(post+"\n","a")
+
+    return post_tmp_file.strpath
