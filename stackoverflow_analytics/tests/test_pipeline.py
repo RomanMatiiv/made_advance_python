dd4bf65e337ebf7eb957430443c0fd358bc4a14b
RomanMatiiv <matiiv.r.a@gmail.com>
2020-12-20 15:41:45 +0300

    write pytest fixture for posts emulate

--- stackoverflow_analytics/tests/test_pipeline.py
+++ stackoverflow_analytics/tests/test_pipeline.py
@@ -3,14 +3,31 @@ import pytest
 from pipeline import Pipeline
 
 @pytest.fixture(scope='function')
-def stackoverflow_posts_path(tmpdir):
-    posts =['<row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="10" Title="Is SEO better better better done with repetition?" />',
-            '<row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="5" Title="What is SEO?" />',
-            '<row PostTypeId="1" CreationDate="2020-01-15T20:31:15.640" Score="20" Title="Is Python better than Javascript?" />',
-            ]
+def stackoverflow_posts(tmpdir):
+    posts = [{"xml": '<row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="10" Title="Is SEO better better better done with repetition?" />',
+              "PostTypeId": 1,
+              "CreationDate": "2019-01-15T20:31:15.640",
+              "Score": 10,
+              "Title": "Is SEO better better better done with repetition?"},
+
+             {"xml": '<row PostTypeId="1" CreationDate="2019-01-15T20:31:15.640" Score="5" Title="What is SEO?" />',
+             "PostTypeId": 1,
+             "CreationDate": "2019-01-15T20:31:15.640",
+             "Score": 5,
+             "Title": "What is SEO?"},
+
+             {"xml": '<row PostTypeId="1" CreationDate="2020-01-15T20:31:15.640" Score="20" Title="Is Python better than Javascript?" />',
+             "PostTypeId": 1,
+             "CreationDate": "2020-01-15T20:31:15.640",
+             "Score": 20,
+             "Title": "Is Python better than Javascript?"}]
+
     post_tmp_file = tmpdir.join("posts.txt")
 
     for post in posts:
-        post_tmp_file.write(post+"\n","a")
+        post_tmp_file.write(post["xml"]+"\n", "a")
+
+    return {"posts": posts,
+            "posts_filepath": post_tmp_file.strpath}
 
     return post_tmp_file.strpath
